4ed917d39b11d46a370fdd9838d9c2541c99d68b
diamat1997 <diamat1997@gmail.com>
2021-04-20 21:25:54 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$(function(){
+    $('.our-clients-slider').slick({
+        arrows: false,
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        variableWidth: true,
+        dots: true,
+        centerMode: true,
+    });
+})
